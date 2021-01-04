ae4de90603b68c540e54982d7dbb3317086746c4
FlyMoe <rrgarcia@gmail.com>
2021-01-03 21:46:27 -0500

    Removed client id

--- src/api/Unsplash.js
+++ src/api/Unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 0kqoVHcSA2jJrliIdu6ctgyD0HWBCHoD8yULREdMArc'
+        Authorization: // Client ID Goes Here
     }
 });
